74679bbe2a7883b32fcccc79a16d99f3204a818e
DALI <sika.dali@telecomnancy.net>
2024-10-26 23:06:30 +0200

    settings page

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,5 +1,31 @@
-const SettingsPage = () => {
-     return <div>Hi Settings!</div>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+import SettingsForm from "./components/settings-form";
+
+interface SettingsPageProps {
+     params: {
+          storeId: string;
+     };
+}
+
+const SettingsPage: React.FC<SettingsPageProps> = async ({ params }) => {
+     const { userId } = auth();
+     if (!userId) redirect("/sign-in");
+
+     const store = await prismadb.store.findFirst({
+          where: { id: params.storeId, userId },
+     });
+     if (!store) redirect("/");
+
+     return (
+          <div className="flex-col">
+               <div className="flex-1 space-y-4 p-8 pt-6">
+                    <SettingsForm initialData={store} />
+               </div>
+          </div>
+     );
 };
 
 export default SettingsPage;
--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: Request) {
 
           // check clerk authentication
           if (!userId) {
-               return new NextResponse("Unauthorized", { status: 401 });
+               return new NextResponse("Unauthenticated", { status: 401 });
           }
 
           if (!name) {
